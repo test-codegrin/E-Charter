d0043ba4ec1c3d6b79e10af0b1c68f9b50789505
utsav0200 <utsavradadiya772@gmail.com>
2025-08-26 17:53:41 +0530

    Remove unused SVG file, update import paths for Quality component, add new image assets for services, and implement Quality component with service descriptions and images.

--- src/app/components/home/Quality.tsx
+++ src/app/components/home/Quality.tsx
@@ -21,7 +21,7 @@ const Quality: FC = () => {
           {/* Left Services */}
           <div className="flex flex-col items-start">
             <div className="w-full max-w-[422px] border-b border-gray-200 flex my-6">
-              <img src="/images/extensive.svg" alt="Extensive Fleet" className="h-[50px]" />
+              <img src="/images/Extensive.png" alt="Extensive Fleet" className="h-[50px]" />
               <div className="ms-6">
                 <p className="text-[20px] font-semibold">Extensive Fleet Options</p>
                 <p className="text-base pb-[20px] font-semibold pt-2 text-[#616161] text-left">
@@ -31,7 +31,7 @@ const Quality: FC = () => {
             </div>
 
             <div className="w-full max-w-[422px] flex">
-              <img src="/images/customer.svg" alt="Customer Focused" className="h-[50px]" />
+              <img src="/images/Customer.png" alt="Customer Focused" className="h-[50px]" />
               <div className="ms-6">
                 <p className="text-[20px] font-semibold">Customer-Focused Experience</p>
                 <p className="text-base font-semibold pt-2 text-[#616161] text-left">
@@ -43,13 +43,13 @@ const Quality: FC = () => {
 
           {/* Middle Image (only on large screens) */}
           <div className="lg:block">
-            <img src="" alt="Quality" className="max-w-full" />
+            <img src="/homepage/quality.png" alt="Quality" className="max-w-full" />
           </div>
 
           {/* Right Services */}
           <div className="flex flex-col items-start">
             <div className="w-full max-w-[422px] border-b border-gray-200 flex my-6">
-              <img src="/images/convenient.svg" alt="Convenient Locations" className="h-[50px]" />
+              <img src="/images/Convenient.png" alt="Convenient Locations" className="h-[50px]" />
               <div className="ms-6">
                 <p className="text-[20px] font-semibold">Convenient Locations</p>
                 <p className="text-base pb-[20px] font-semibold pt-2 text-[#616161] text-left">
@@ -59,7 +59,7 @@ const Quality: FC = () => {
             </div>
 
             <div className="w-full max-w-[422px] flex">
-              <img src="/images/reliability.svg" alt="Reliability & Safety" className="h-[50px]" />
+              <img src="/images/Reliability.png" alt="Reliability & Safety" className="h-[50px]" />
               <div className="ms-6">
                 <p className="text-[20px] font-semibold">Reliability and Safety</p>
                 <p className="text-base font-semibold pt-2 text-[#616161] text-left">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "./components/home/Hero";
 import Choice from "./components/home/Choice";
 import Service from "./components/home/Service";
 import Fleet from "./components/home/Fleet";
-import Quality from "../../public/images/Quality";
+import Quality from "./components/home/Quality";
 import BookCar from "./components/home/BookCar";
 
 export default function Page() {
